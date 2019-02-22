b9d491ec1b5156856c32e638aaa6a532e2e54163
rupak <rupak4@yandex.ru>
2019-02-22 11:45:30 +0300

    Создание категории блога + пост блога

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.7.27 on 2019-02-21 20:43:17.
+ * Generated for Laravel 5.7.27 on 2019-02-22 07:53:40.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
