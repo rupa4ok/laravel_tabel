3dbb9163a7ad88cfa313b00400a9828815c27f96
rupak <rupak4@yandex.ru>
2019-02-25 16:42:01 +0300

    TestController

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::resource('rest', 'RestTestController')->names('restTest');
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
